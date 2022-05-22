cabdd0f93cc10d83e7b719f5148951e1b7aba439
Hong Minhee <hong.minhee@gmail.com>
2022-05-23 01:34:11 +0900

    assertStreamStartsWith() function

--- testing.ts
+++ testing.ts
@@ -14,3 +14,22 @@ export async function assertStreams<T>(
   for await (const value of actual) elements.push(value);
   assertEquals(elements, expected);
 }
+
+/**
+ * Makes an assertion that the beginning elements of the `actual` iterable are
+ * in common with the elements of the `expected` array.
+ * @param actual The async iterable to compare.  It can be either finite or
+ *               infinite.
+ * @param expected The array that contains the expected beginning elements.
+ */
+export async function assertStreamStartsWith<T>(
+  actual: AsyncIterable<T>,
+  expected: T[],
+): Promise<void> {
+  const elements: T[] = [];
+  for await (const value of actual) {
+    elements.push(value);
+    if (elements.length >= expected.length) break;
+  }
+  assertEquals(elements.slice(0, expected.length), expected);
+}
--- testing_testing.ts
+++ testing_testing.ts
@@ -2,7 +2,7 @@ import {
   AssertionError,
   assertRejects,
 } from "https://deno.land/std@0.140.0/testing/asserts.ts";
-import { assertStreams } from "./testing.ts";
+import { assertStreams, assertStreamStartsWith } from "./testing.ts";
 
 async function* getAsyncIterable<T>(
   ...args: T[]
@@ -12,12 +12,78 @@ async function* getAsyncIterable<T>(
   }
 }
 
+async function* getInfiniteIterable(): AsyncIterableIterator<number> {
+  for (let i = 0; true; i++) yield i;
+}
+
 Deno.test("assertStreams()", async () => {
   await assertStreams(getAsyncIterable<unknown>(), []);
   await assertStreams(getAsyncIterable(1, 9, 8, 4), [1, 9, 8, 4]);
   await assertStreams(getAsyncIterable("foo", "bar"), ["foo", "bar"]);
   await assertRejects(
     () => assertStreams(getAsyncIterable(1, 9, 8, 7), [1, 9, 8, 4]),
+    AssertionError,
+  );
+});
+
+Deno.test("assertStreamStartsWith()", async () => {
+  await assertStreamStartsWith(getAsyncIterable<unknown>(), []);
+  await assertStreamStartsWith(getAsyncIterable(1, 9, 8, 4), []);
+  await assertStreamStartsWith(getAsyncIterable(1, 9, 8, 4), [1]);
+  await assertStreamStartsWith(getAsyncIterable(1, 9, 8, 4), [1, 9]);
+  await assertStreamStartsWith(getAsyncIterable(1, 9, 8, 4), [1, 9, 8]);
+  await assertStreamStartsWith(getAsyncIterable(1, 9, 8, 4), [1, 9, 8, 4]);
+  await assertStreamStartsWith(getAsyncIterable("foo", "bar"), []);
+  await assertStreamStartsWith(getAsyncIterable("foo", "bar"), ["foo"]);
+  await assertStreamStartsWith(getAsyncIterable("foo", "bar"), ["foo", "bar"]);
+  await assertStreamStartsWith(getInfiniteIterable(), [0]);
+  await assertStreamStartsWith(getInfiniteIterable(), [0, 1]);
+  await assertStreamStartsWith(getInfiniteIterable(), [0, 1, 2]);
+  await assertStreamStartsWith(getInfiniteIterable(), [0, 1, 2, 3]);
+  await assertRejects(
+    () => assertStreamStartsWith(getAsyncIterable(), [1]),
+    AssertionError,
+  );
+  await assertRejects(
+    () => assertStreamStartsWith(getAsyncIterable(1, 9, 8, 4), [9]),
+    AssertionError,
+  );
+  await assertRejects(
+    () => assertStreamStartsWith(getAsyncIterable(1, 9, 8, 4), [1, 9, 9]),
+    AssertionError,
+  );
+  await assertRejects(
+    () => assertStreamStartsWith(getAsyncIterable(1, 9, 8, 4), [1, 9, 8, 7]),
+    AssertionError,
+  );
+  await assertRejects(
+    () => assertStreamStartsWith(getAsyncIterable(1, 9, 8, 4), [1, 9, 8, 4, 0]),
+    AssertionError,
+  );
+  await assertRejects(
+    () => assertStreamStartsWith(getAsyncIterable("foo", "bar"), ["bar"]),
+    AssertionError,
+  );
+  await assertRejects(
+    () =>
+      assertStreamStartsWith(getAsyncIterable("foo", "bar"), ["foo", "baz"]),
+    AssertionError,
+  );
+  await assertRejects(
+    () =>
+      assertStreamStartsWith(getAsyncIterable("foo", "bar"), [
+        "foo",
+        "bar",
+        "baz",
+      ]),
+    AssertionError,
+  );
+  await assertRejects(
+    () => assertStreamStartsWith(getInfiniteIterable(), [1]),
+    AssertionError,
+  );
+  await assertRejects(
+    () => assertStreamStartsWith(getInfiniteIterable(), [0, 1, 2, 9]),
     AssertionError,
   );
 });
