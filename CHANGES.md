6247a23f1d7babb2a00a25a1e2c50cb46a5e4002
Hong Minhee <hong.minhee@gmail.com>
2022-05-23 12:43:32 +0900

    Re-export everything

--- CHANGES.md
+++ CHANGES.md
@@ -8,6 +8,8 @@ Version 0.2.0
 
 To be released.
 
+ -  Added *mod.ts* module which re-exports everything from other modules.
+
 
 Version 0.1.0
 -------------
---
+++ mod.ts
@@ -0,0 +1,4 @@
+export { toArray } from "./collections.ts";
+export { count } from "./infinite.ts";
+export { take, takeWhile } from "./take.ts";
+export { assertStreams, assertStreamStartsWith } from "./testing.ts";
